a2ee2b77d3e175875778404100afaa5ef0e7c6e0
swfz <sawafuji.09@gmail.com>
2020-05-31 05:24:55 +0900

    add configurationfile

---
+++ renovate-config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  branchPrefix: 'self-renovate/',
+  dryRun: true,
+  gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+  logLevel: 'debug',
+  onboarding: false,
+  platform: 'github',
+  includeForks: true,
+  repositories: [
+      'swfz/github-actions-playground'
+    ],
+  packageRules: [
+      {
+            description: 'lockFileMaintenance',
+            updateTypes: [
+                    'pin',
+                    'digest',
+                    'patch',
+                    'minor',
+                    'major',
+                    'lockFileMaintenance',
+                  ],
+            masterIssueApproval: false,
+            stabilityDays: 0,
+          },
+    ],
+};
