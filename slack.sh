5d34d49fac8b8c53bc86ba415437db75c03c4521
swfz <sawafuji.09@gmail.com>
2020-09-23 16:51:26 +0900

    send webhook

--- slack.sh
+++ slack.sh
@@ -10,35 +10,37 @@
 # ref
 
 
-workflow=$1
-actor=$2
-event_name=$3
-action=$4
+color=$1
+workflow=$2
+actor=$3
+event_name=$4
+action=$5
 
+echo $color
 echo $workflow
 echo $actor
 echo $event_name
 echo $action
 
-#curl -X POST -H 'Content-type: application/json' ${SLACK_WEBHOOK_URL} \
-#  -d @- <<EOS
-#{
-#    "attachments": [
-#      {
-#        "color": "#36a64f",
-#        "fallback": "Required plain-text summary of the attachment.",
-#        "blocks": [
-#          {
-#            "type": "context",
-#            "elements": [
-#              {
-#                "type": "mrkdwn",
-#                "text": "Location: *Dogpatch* <https://github.com/swfz|swfz>)"
-#              }
-#            ]
-#          }
-#        ]
-#      }
-#    ]
-#}
-#EOS
+curl -X POST -H 'Content-type: application/json' ${SLACK_WEBHOOK_URL} \
+  -d @- <<EOS
+{
+    "attachments": [
+      {
+        "color": "#36a64f",
+        "fallback": "Required plain-text summary of the attachment.",
+        "blocks": [
+          {
+            "type": "context",
+            "elements": [
+              {
+                "type": "mrkdwn",
+                "text": "Location: *Dogpatch* <https://github.com/swfz|swfz>)"
+              }
+            ]
+          }
+        ]
+      }
+    ]
+}
+EOS
